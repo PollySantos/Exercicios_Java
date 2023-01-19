921128abb36f757cef8b172ffd2abb32933b2b86
Polly Santos <99842806+PollySantos@users.noreply.github.com>
2023-01-18 22:35:53 -0300

    ajustando sintaxe

--- Fundamentos da Plataforma Java/src/Classes/Exercicio01.java
+++ Fundamentos da Plataforma Java/src/Classes/Exercicio01.java
@@ -2,6 +2,6 @@ package Classes;
 
 public class Exercicio01 {
     public static void main (String [] args) {
-        System.out.print ("Olá, tudo bem?\nHoje 18/01/2023, estarei voltando aos estudos com foco em Java?\nVem meu sim");
+        System.out.print ("Olá, tudo bem?\nHoje 18/01/2023, estarei voltando aos estudos com foco em Java?\nLet's go!");
     }
 }
