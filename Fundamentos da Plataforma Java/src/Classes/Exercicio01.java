33b52620ca82cc51f0922ec1e826da043a462073
Polly Santos <99842806+PollySantos@users.noreply.github.com>
2023-01-18 22:30:19 -0300

    inserindo exercicio 1 e 2

---
+++ Fundamentos da Plataforma Java/src/Classes/Exercicio01.java
@@ -0,0 +1,7 @@
+package Classes;
+
+public class Exercicio01 {
+    public static void main (String [] args) {
+        System.out.print ("Olá, tudo bem?\nHoje 18/01/2023, estarei voltando aos estudos com foco em Java?\nVem meu sim");
+    }
+}
---
+++ Fundamentos da Plataforma Java/src/Classes/Exercicio02.java
@@ -0,0 +1,13 @@
+package Classes;
+
+public class Exercicio02 {
+    public static void main (String [] args) {
+    int ano = 2022;
+    ano = 2023;
+    final String BR = "Brasil";
+
+        System.out.println(BR + "\n" + ano);
+
+    }
+
+}
